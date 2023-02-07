fd4e41cb1f52f46564c7ab4d3a5c16b19acf6470
Gitesh054 <giteshpatidar16@gmail.com>
2023-02-07 21:27:13 +0530

    Third commit

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from main import db
 
 
 class pricetracker(db.Model):
-    url = db.Column(db.String(2000), primary_key=False, unique=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
+    url = db.Column(db.String(2000), unique=False)
     name = db.Column(db.String(2000), unique=False, nullable=False)
     price = db.Column(db.String(10), unique=False, nullable=False)
-    id = db.Column(db.Integer,primary_key=True ,unique=True, nullable=False)
     date = db.Column(db.String(20), unique=False, nullable=False)
     time = db.Column(db.String(20), unique=False, nullable=True)
